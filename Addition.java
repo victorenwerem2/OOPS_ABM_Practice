272699d74db08d98fe8f04dd192c45252c5ffc0d
Abrahamtekle <162491486+Abrahamtekle@users.noreply.github.com>
2026-04-13 20:37:50 -0600

    Add Multiplication class to perform multiplication

--- Addition.java
+++ Addition.java
@@ -15,3 +15,12 @@ public class Subtraction {
         System.out.println("Result = " + result);
     }
 }
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = a * b;
+        System.out.println("Result = " + result);
+    }
+}
